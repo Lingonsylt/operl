993d95f6509e709cee3732379d4fa22688757bcc
lingon <anton@naetet.se>
2013-11-05 10:50:39 +0100

    changed from numeric keys to hashes in module key

--- src/chord/key.erl
+++ src/chord/key.erl
@@ -1,10 +1,11 @@
 -module(key).
 
 %% API
--export([generate/0,between/3]).
+-export([generate/0,between/3, hexhash/1]).
 
 generate() ->
-  random:uniform(1000000000).
+  Hash = hexhash(),
+  lists:nthtail(length(Hash) - 4, Hash).
 
 between(Key, From, To) ->
   if
@@ -16,4 +17,8 @@ between(Key, From, To) ->
       (Key > From) or (Key =< To)
   end.
 
+hexhash() ->
+  hexhash(integer_to_list(random:uniform(1000000000))).
+hexhash(Str) ->
+  lists:flatmap(fun(X) -> integer_to_list(X, 16) end, binary_to_list(crypto:hash(sha, Str))).
 
