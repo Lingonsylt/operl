d37fee3d868acb5625f5a5f06c251fa2ae4d3cb8
Anton <anton@naetet.se>
2013-11-05 14:56:59 +0100

    added documentation to module key

--- src/chord/key.erl
+++ src/chord/key.erl
@@ -1,12 +1,12 @@
 -module(key).
-
-%% API
 -export([generate/0,between/3, hexhash/1]).
 
 generate() ->
   Hash = hexhash(),
   lists:nthtail(length(Hash) - 4, Hash).
 
+% Svara på om en nyckel är mellan två andra nycklar i en ring, exklusive From, inklusive To.
+% Eftersom det är en ring så är alla nycklar mellan X och X, och alla utom 1 mellan 1 och 0.
 between(Key, From, To) ->
   if
     To == From ->
